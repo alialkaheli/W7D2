0e52ee3d206b18a5ef880bafead24d544278f673
ali alkaheli <alialkaheli@alis-MacBook-Pro.local>
2018-12-04 17:37:42 -0800

    setup root - static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     get 'todos/update'
     get 'todos/destroy'
   end
+  root to: 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
